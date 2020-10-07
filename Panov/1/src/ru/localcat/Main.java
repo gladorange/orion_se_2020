830c7bfb2d762102d8eb4b5b721d4350f7f54f3c
maximpanov <maksim.panov@timebook.ru>
2020-10-07 17:40:50 +0300

    #1.1.1 Написать программу, в которой есть две переменные типа Integer со значениями. Значения можно указать в коде программы или ввести с клавиатуры или взять из аргументов.

--- Panov/1/src/ru/localcat/Main.java
+++ Panov/1/src/ru/localcat/Main.java
@@ -1,8 +1,37 @@
 package ru.localcat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Formatter;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+
+        System.out.println("Input first value");
+        String sValue1 = reader.readLine();
+        Integer value1 = Integer.parseInt(sValue1);
+
+        System.out.println("Input second value:");
+        String sValue2 = reader.readLine();
+        Integer value2 = Integer.parseInt(sValue2);
+
+        if(!value1.equals(value2)) {
+            Formatter formatter = new Formatter();
+            if(value1 > value2) {
+                formatter.format("Число %s больше %s", value1, value2);
+            }
+            else {
+                formatter.format("Число %s меньше %s", value1, value2);
+            }
+            System.out.println(formatter);
+        }
+        else {
+            System.out.println("Введные числа равны");
+        }
+
+        System.out.println("Сумма чисел = " + (value1 + value2));
     }
 }
