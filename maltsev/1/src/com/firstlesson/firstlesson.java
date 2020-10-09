89cdc5aa60feb8a48280b728148ca43cb60d9e51
Andrey Maltsev <andrei@maltsev.me>
2020-10-09 23:35:33 +0300

    First ex. complete

--- maltsev/1/src/com/firstlesson/firstlesson.java
+++ maltsev/1/src/com/firstlesson/firstlesson.java
@@ -14,8 +14,10 @@ public class firstlesson {
         System.out.println("Введите число 2: ");
         int in2 = in.nextInt();
 
-        String output = String.format("Число %s больше %s", Math.max(in1,in2), Math.min(in1,in2));
-        System.out.println(output);
+        if (in1 - in2 != 0) {
+            String output = String.format("Число %s больше %s", Math.max(in1, in2), Math.min(in1, in2));
+            System.out.println(output);
+        }
 
         System.out.println(in1 + in2    );
 
