8400318f9fc5ba91a29b82aa63d2a56437e06903
Andrey Maltsev <andrei@maltsev.me>
2020-10-09 23:35:33 +0300

    Не увидел в задании больше или меньше, что надо выдать две разные строки, а не "какое число больше другого".

--- maltsev/1/src/com/firstlesson/firstlesson.java
+++ maltsev/1/src/com/firstlesson/firstlesson.java
@@ -14,8 +14,15 @@ public class firstlesson {
         System.out.println("Введите число 2: ");
         int in2 = in.nextInt();
 
-        String output = String.format("Число %s больше %s", Math.max(in1,in2), Math.min(in1,in2));
-        System.out.println(output);
+        if (in1 > in2)
+        {
+            System.out.println(String.format("Число %s больше %s", in1, in2));
+        }
+
+        if (in1 < in2)
+        {
+            System.out.println(String.format("Число %s меньше %s", in1, in2));
+        }
 
         System.out.println(in1 + in2    );
 
