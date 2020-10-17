349410f066d4a04e73e85e12e996b18aa82c6c9d
EvgenyDruzhinin <ev_dru@mail.ru>
2020-10-17 21:28:06 +0300

    [HomeWork2][MagicOfNumbers]
    - corrected random generate algorithm
    - added ARRAY_SIZE variable
    - used arrayOfIntegers.length in for cycle

--- Druzhinin/2/src/MagicOfNumbers/MagicOfNumbers.java
+++ Druzhinin/2/src/MagicOfNumbers/MagicOfNumbers.java
@@ -1,17 +1,18 @@
 package MagicOfNumbers;
 
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class MagicOfNumbers {
-    public static void main (String[] args) {
-        int [] arrayOfIntegers = new int[100];
+    private final static int ARRAY_SIZE = 100;
 
-        for (int i = 0; i < 100; i++) {
+    public static void main (String[] args) {
+        int [] arrayOfIntegers = new int[ARRAY_SIZE];
+
+        for (int i = 0; i < arrayOfIntegers.length; i++) {
             Random random = new Random();
-            int generatedInteger = random.nextInt(101)  - (random.nextInt(2) == 0 ? 0 : 100);
+            int generatedInteger = random.nextInt(ARRAY_SIZE * 2 - 1) - ARRAY_SIZE;
+
             arrayOfIntegers[i] = generatedInteger;
-            // System.out.println("Число: " + generatedInteger);
             if(isMagicNumber(generatedInteger)) {
                 System.out.printf("Число %d - магическое!\n", arrayOfIntegers[i]);
             }
@@ -21,7 +22,6 @@ public class MagicOfNumbers {
     public static boolean isMagicNumber(int number) {
         int whole = number / 10;
         int fractional = number % 10;
-        // System.out.println("whole: " + whole + " fractional: " + fractional);
 
         if (whole == fractional) {
             if (Math.abs(whole) > 0 && Math.abs(whole) < 100) {
