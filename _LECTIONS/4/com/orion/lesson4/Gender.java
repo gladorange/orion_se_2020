b17cf815093e141029afe3f1632249d3ea72c6b9
atarasov <atarasov@mera.ru>
2020-10-20 18:06:25 +0300

    Lection 3 fix

--- _LECTIONS/4/com/orion/lesson4/Gender.java
+++ _LECTIONS/4/com/orion/lesson4/Gender.java
@@ -1,20 +1,26 @@
 package com.orion.lesson4;
 
 public enum Gender {
-    MALE("Мужской"),
-    FEMALE("Женский");
-
+    MALE("Мужской","Сеньор"),
+    FEMALE("Женский","Мадам");
 
     final private String title;
+    final private String greeting;
 
-    Gender(String title) {
+    Gender(String title, String greeting) {
         this.title = title;
+        this.greeting = greeting;
     }
 
 
     @Override
     public String toString() {
         return title;
+    }
+
+
+    public String getGreeting() {
+        return greeting;
     }
 }
 
