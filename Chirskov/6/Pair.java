cebb69e031b32a360715b4bdbeddb06310961e3b
AlexeyChirskov <alexeychirskov@gmail.com>
2020-10-27 02:55:49 +0300

    lesson 6

---
+++ Chirskov/6/Pair.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Pair<T, E> {
+    private T first;
+    private E second;
+    Pair(T first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(E second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(first).append(" ").append(second).toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?,?> pair = (Pair<?,?>) o;
+        if (first != pair.first)
+            return false;
+        if (second != pair.second)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
