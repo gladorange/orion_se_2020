8e61e569aad539dae1784af5d9b6f3a007d85952
maximpanov <maksim.panov@timebook.ru>
2020-11-23 20:17:59 +0300

    #hw14 - init commit

---
+++ Panov/hw14/src/main/java/ru/localcat/examples/Dog.java
@@ -0,0 +1,10 @@
+package ru.localcat.examples;
+
+import ru.localcat.fwcore.annotations.SimpleComponent;
+
+@SimpleComponent
+public class Dog {
+    void gav() {
+        System.out.println("Гав-гав!");
+    }
+}
