3cd530a70b759f8b029515602af12a451e785e8c
GilgameshH0 <oleg.miracle@yandex.ru>
2020-11-23 19:10:31 +0300

    fixed last task

--- Shilkin/11/src/main/java/shilkin/SimpleDIFramework.java
+++ Shilkin/11/src/main/java/shilkin/SimpleDIFramework.java
@@ -9,11 +9,13 @@ import shilkin.exceptions.NotAnnotatedWithSimpleComponentException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class SimpleDIFramework {
     public SimpleDIFramework(String basePackagesToScan) throws NotAnnotatedWithSimpleComponentException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
         Set<Class<?>> classes = new Reflections(basePackagesToScan).getTypesAnnotatedWith(SimpleComponent.class);
+        ArrayList<Object> fieldObjects = new ArrayList<>();
         for (Class<?> aClass : classes) {
             Object object = aClass.getDeclaredConstructor().newInstance();
             for (Field declaredField : aClass.getDeclaredFields()) {
@@ -22,7 +24,14 @@ public class SimpleDIFramework {
                         throw new NotAnnotatedWithSimpleComponentException("Класс, не содержится в контейнере.");
                     }
                     declaredField.setAccessible(true);
-                    declaredField.set(object, declaredField.getType().getDeclaredConstructor().newInstance());
+                    Object fieldObject = declaredField.getType().getDeclaredConstructor().newInstance();
+                    if (!fieldObjects.contains(fieldObject)) {
+                        fieldObjects.add(fieldObject);
+                        declaredField.set(object, fieldObject);
+                    } else {
+                        int index = fieldObjects.indexOf(fieldObject);
+                        declaredField.set(object, fieldObjects.get(index));
+                    }
                 }
             }
             for (Method declaredMethod : aClass.getDeclaredMethods()) {
